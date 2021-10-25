30f0adb29004dd27aeec065a8cb4339b01395d73
Shota Shirai <shotashirai@gmail.com>
2021-10-25 21:52:23 +1300

    Add description

--- train_neuralnet.py
+++ train_neuralnet.py
@@ -1,3 +1,26 @@
+# coding: utf-8
+
+"""
+Code: train_neuralnet.py
+Author: Shota Shirai
+Input: mnist handwritten digit data (in 'data' directory)
+Output: 
+    - model.cpkl (option): trained network objects
+    - params.pkl (option): weights and biases at an epoch
+Required class/functions are provided by 'NetworkElements'
+
+This code generates a multilayer neural network and trains it. In this code, MNIST
+handwritten digit data is used for training and testing the built network. 
+The trained network and trained parameters (weights and biases) are saved in pickle 
+(or cloudpickle) files.
+
+Note: Tested Python version 3.7.5
+
+"""
+
+###################################################################################
+# Import libraries 
+###################################################################################
 import sys, os
 sys.path.append(os.pardir)
 import numpy as np
@@ -5,9 +28,14 @@ from dataset.preprocess_mnist import load_mnist
 from NetworkElements.multi_layer_net import MultiLayerNetwork
 from NetworkElements.trainer import *
 
-# import data
+###################################################################################
+# Import data 
+###################################################################################
 (x_train, t_train), (x_test, t_test) = load_mnist(normalize=True, one_hot_label=True)
 
+###################################################################################
+# Generate network
+###################################################################################
 # hidden layers
 hidden_size = [50, 50, 50]
 # Generate network
@@ -17,10 +45,13 @@ network = MultiLayerNetwork(
     , output_size=10
     )
 
+###################################################################################
+# Train network
+###################################################################################
 trainer = Trainer(network, x_train, t_train, x_test, t_test
                 , epochs=20, verbose=2, learning_rate=0.1
                 , save_network=True, save_params_epoch=5)
-
 trainer.train()
+
 
 #TODO Visualization of the leraning process
